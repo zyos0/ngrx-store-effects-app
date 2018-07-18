2f67edec97b8aa079f68550242deaeb9103ea566
Roberto Malaver <robertomalaver7@gmail.com>
2018-07-18 15:10:30 -0500

    09  - Toppings Action  Creators

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -38,6 +38,7 @@ export class ProductItemComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.store.dispatch(new fromStore.LoadToppings());
     this.pizza$ = this.store.select(fromStore.getSelectedPizza);
   }
 
